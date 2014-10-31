ba17ff6d68775910e62745b07ce6f931e1d2883d
Michael Owens <mk@mowens.com>
2014-10-31 16:53:08 -0700

    v0.0.1 - Starting up the Style Guide.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ChooseUX
 ========
 
-User Experience Prototyping Repository
+User Experience Prototyping Repository and Style Guide
+
+Get Started
+===========
+
+TODO: Write instructions on how to run it. tl;dr: install Compass, Node, npm install, and run node static.js
---
+++ static.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var port = parseInt(process.env.CE_UX_PORT, 10) || 5585;
+
+var app = express();
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(methodOverride());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/lib'));
+
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+app.listen(port)
