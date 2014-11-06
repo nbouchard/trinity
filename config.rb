d40b503ada00e043f6a2dd860db84240585b9032
Michael Owens <mk@mowens.com>
2014-11-05 23:45:20 -0800

    Updates to using ChooseStyles submodule.

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 require 'compass/import-once/activate'
 require 'breakpoint'
+require 'bootstrap-sass'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
