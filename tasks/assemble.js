ff19e7a40a17de7d686fcfaed279838741e755db
Matt Seymour <seymour@mobiah.com>
2015-12-04 16:09:33 -0800

    Add new template files. Get assemble task to duplicate lib folder structure within list.

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -21,15 +21,29 @@ module.exports = function(grunt) {
     grunt.config('assemble', {
         options: {
             flatten: true,
+            // expand: true,
             partials: ['lib/templates/partials/*.html','lib/templates/partials/**/*.html'],
             layoutext: '.html',
-            layoutdir: 'lib/templates/layouts',
+            layoutdir: 'lib/templates/layouts/',
             layout: 'default',
-            data: ['lib/templates/data/*.json', 'lib/templates/data/**/*.json'],
+            data: ['lib/templates/data/*.json','lib/templates/data/**/*.json'],
         },
         pages: {
-            src: ['lib/templates/*.html'],
-            dest: 'dist/'
+            // src: ['lib/templates/*.html'],{,*/}*
+            // src: ['lib/templates/pages/*.html','lib/templates/pages/**/*.html'],
+            // dest: 'dist/'
+            // options: {
+
+            // },
+            files: [
+                {
+                    expand: true,
+                    cwd: 'lib/templates/pages/',
+                    src: ['*.html','**/*.html'],
+                    dest: 'dist/',
+                    // ext: '.html'
+                }
+            ]
         }
     });
 
